8fb39acb7d53d672ae4bb30194dbefd266b71c18
NicolasTfile <nicolastfile@gmail.com>
2023-01-12 06:13:39 -0800

    Made more corrections to more malloc and free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,11 +10,17 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-        char *c;
-        int i;                                             
-        if (nmemb == 0 || size == 0)
-                return (NULL);
-        c = malloc(nmemb * size);                                  if (c == NULL)                                                     return (NULL);
-        for (i = 0; i < (nmemb * size); i++)
-                *(c + i) = 0;
-        return (c);                                        }
+	char *c;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	c = malloc(nmemb * size);
+
+	if (c == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		*(c + i) = 0;
+
+	return (c);
+}
