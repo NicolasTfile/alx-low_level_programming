8c86b7cbe55c7768aedaf81f1066158f266b104a
NicolasTfile <nicolastfile@gmail.com>
2023-01-18 07:56:42 -0800

    Created and fixed function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,19 +1,11 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
 
+#include <stddef.h>
+
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-/**
- * enum bool - typedef function for bool
- * @TRUE: 1
- * @FALSE: 0
-*/
-typedef enum bool
-{
-	TRUE = 1,
-	FALSE = 0
-} bool;
-
-#endif /* FUNCTION_POINTERS_H */
+#endif/* FUNCTION_POINTERS_H*/
