87d105c67823ad5df5398ba3a6c2f9f5c44cc4eb
NicolasTfile <nicolastfile@gmail.com>
2023-01-18 08:17:20 -0800

    Created and fixed function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -13,23 +13,23 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	/* struct opts of struct op_t */
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
 	};
 	int i = 0;
 
 	while (i < 5)
 	{
-		if (*s == *ops[i].op)
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
+
 		i++;
 	}
 
-	return (NULL);
+	return (0);
 }
